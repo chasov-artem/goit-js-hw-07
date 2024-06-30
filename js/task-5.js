e7c459e6aee188fc8af233562c0eb4c2fddbd9ef
chasov-artem <chasov90@gmail.com>
2024-06-30 16:12:54 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const changeButton = document.querySelector(".change-color");
+const bodyBack = document.querySelector(".color");
+
+
+changeButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  bodyBack.textContent = randomColor;
+})
